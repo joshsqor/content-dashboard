b5b8f16f7ad19007d16bab446a22f282009c0dea
Josh Ballesteros <josh@sqor.com>
2014-06-11 17:06:57 -0700

    better naming convention and comments in file

---
+++ ap_dashboard.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+#Content Dashboard
+
+"""
+ex usage: fetch_count(item_type="AP", start_date=[2014,6,1], end_date=[2014,6,2])
+
+valid types are:
+- AP
+- twitter
+- instagram
+- getty_images
+- espn_api
+- rss
+- sqor
+- curated
+- comment
+
+"""
+
+from datetime import datetime
+import requests
+import json
+
+URL = "http://feedtools.sqor.com/content?q=type:%(item_type)s AND published_dt:[%(start_date)s TO %(end_date)s]&limit=0"
+
+def fetch_count(item_type, start_date, end_date=datetime.now()):
+    start_date = check_date(start_date)
+    end_date = check_date(end_date)
+    res = requests.get(URL % { "item_type":item_type
+                             , "start_date":start_date
+                             , "end_date": end_date})
+    total = json.loads(res.content).get("total")
+    #print total
+    return total
+
+def check_date(adate):
+    if isinstance(adate, list):
+        return dater(adate[0], adate[1], adate[2])
+    else:
+        return isofixer(adate)
+
+def dater(year, month, day):
+    return isofixer(datetime(year, month, day))
+
+def isofixer(adate):
+    return datetime.isoformat(adate).split('.')[0] + "Z"
+
+
+def report():
+     """This creates a report"""
+
+     article_count = fetch_count(item_type="AP", start_date=[2014,6,1], end_date=[2014,6,2])
+     print "This is the article count ", article_count
+     return None
+
+
+
+
+
+
+
